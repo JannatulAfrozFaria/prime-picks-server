89d6dee383a33afafc410bd64d60c7f2be0df27c
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-08-16 01:46:01 +0600

    express,cors required in index.js file

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 //middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(res,req)=>{
+    res.setEncoding('prime picks is running')
+})
