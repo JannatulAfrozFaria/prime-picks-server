6bc1450cac87846beae0ee5e612a200e99037dd2
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-08-16 03:06:51 +0600

    get api created fror medicines

--- index.js
+++ index.js
@@ -25,6 +25,12 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const medicineCollection = client.db("primePicksDB").collection("medicines");
+
+    app.get('/medicines', async(req,res)=>{
+        const result = await medicineCollection.find().toArray();
+        res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
