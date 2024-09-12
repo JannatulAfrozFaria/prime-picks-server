f0530aa94d24f091e3270aa21d000c1178205363
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-13 01:15:06 +0600

    medicine details api created

--- index.js
+++ index.js
@@ -66,6 +66,14 @@ async function run() {
       )
     })
 
+    // medicine -----DETAILS------
+    app.get('/medicine/:id', logger, async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await medicineCollection.findOne(query);
+      res.send(result);
+  })
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
